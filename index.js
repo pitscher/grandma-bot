abe58d0de84d8c586de5e197c0efa82c9b217494
pitscher <mail@hn-goering.de>
2020-05-30 00:58:37 +0200

    Add read msg file and get random msg functionality

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-//const express = require('express');
-//const bodyParser = require('body-parser');
 const fs = require('fs');
-
-//const app = express;
-//const port = 8000;
-
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
-
-//app.listen(port, () => {
-//  console.log(`App running on port ${port}`);
-//});
-
-//module.exports = app;
 
 // Read line by line of messages.txt and create an object of all messages
 var messages = fs.readFileSync('messages.txt', 'utf8').split('\n');
