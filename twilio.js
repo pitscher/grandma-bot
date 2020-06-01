7b2b425de0c8cd2e73e2e97790b7596d2bb61e43
pitscher <mail@hn-goering.de>
2020-06-01 15:13:31 +0200

    Implement promise to catch twilio errors

--- twilio.js
+++ twilio.js
@@ -6,13 +6,20 @@ const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const sendMessageAsSms = (messageReceiver, selectedMessage) => {
   console.log('[i] Sending message - hold tight...');
   const client = require('twilio')(twilioAccountSid, twilioAuthToken);
-  client.messages
+  var promise = client.messages
     .create({
       body: selectedMessage,
       from: process.env.TWILIO_PHONE_NUMBER,
       to: messageReceiver
-    })
-    .then(message => console.log(message.sid + '\n' + '[i] Message sent successfully to ' + message.to));
+    });
+  //.then(message => console.log(message.sid + '\n' + '[i] Message sent successfully to ' + message.to));
+
+  promise.then(function (message) {
+    console.log('Success! SMS SID: ' + message.sid);
+  }, function (error) {
+    console.log('Failed! Reason: ' + error.message);
+  });
+
 };
 
 module.exports = sendMessageAsSms;
