e3fa9c3f19f8e610599880b62c1d4a617303c2ec
pitscher <mail@hn-goering.de>
2020-06-01 16:36:49 +0200

    Change twilio success and error output slightly

--- twilio.js
+++ twilio.js
@@ -12,12 +12,11 @@ const sendMessageAsSms = (messageReceiver, selectedMessage) => {
       from: process.env.TWILIO_PHONE_NUMBER,
       to: messageReceiver
     });
-  //.then(message => console.log(message.sid + '\n' + '[i] Message sent successfully to ' + message.to));
 
   promise.then(function (message) {
-    console.log('Success! SMS SID: ' + message.sid);
+    console.log('[OK] Success! SMS-SID: ' + message.sid);
   }, function (error) {
-    console.log('Failed! Reason: ' + error.message);
+    console.log('[FAIL] There was an error! Detail:\n' + error.message);
   });
 
 };
